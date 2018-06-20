73b4861d5762a353c8e3fad12b7f880e7bd77175
floathandthing <springtrap.bite87@gmail.com>
2018-06-20 18:13:36 +0100

    Adds necesscary reference and foundation for future stuff

--- CMD Music Player/Program.cs
+++ CMD Music Player/Program.cs
@@ -4,8 +4,18 @@ namespace CMD_Music_Player
 {
     class Program
     {
+        public static WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer(); //the namesake of this program
         static void Main(string[] args)
         {
+            string filepath = @"C:\Users\USERNAME\Music\eyyy it works.mp3";
+            try
+            {
+                Console.WriteLine("Starting playback of " + filepath);
+                wplayer.URL = filepath;
+                wplayer.controls.play();
+                Console.ReadLine();
+            }
+            catch (Exception err) { Console.WriteLine("Error playing file: " + err.Message); }
         }
     }
 }
