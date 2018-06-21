cc505afed501bd1e10e0db122777b8b87a4a29a9
floathandthing <springtrap.bite87@gmail.com>
2018-06-21 09:40:00 +0100

    Amalgamates Program and functions class, as there is no good reason for the code to be separated.

--- CMD Music Player/Program.cs
+++ CMD Music Player/Program.cs
@@ -5,7 +5,7 @@ namespace CMD_Music_Player
 {
     class Program
     {
-        public static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer(); //the namesake of this program
+        public static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer(); //the heart of this program
         public static void error(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -27,44 +27,33 @@ namespace CMD_Music_Player
                     foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory()))
                         Console.WriteLine("\t" + Path.GetFileName(file));
                 }
-                else if (commandupper == "PLAY") { functions.resume(player); }
+                else if (commandupper == "PLAY") { player.controls.play(); }
                 else if (commandupper.StartsWith("PLAY "))
                 {
                     string path = command.Substring(5);
                     if (File.Exists(path))
                     {
                         Console.WriteLine("Playing " + path + "...");
-                        functions.play(player, path);
+                        try
+                        {
+                            player.URL = path;
+                            player.controls.play();
+                        }
+                        catch (Exception err) { error("Error playing file: " + err.Message); }
                     }
                     else { error("Error playing file: File doesn't exist."); }
                 }
-                else if (commandupper.StartsWith("STOP")) { functions.stop(player); }
-                else if (commandupper.StartsWith("PAUSE")) { functions.pause(player); }
+                else if (commandupper.StartsWith("STOP")) { player.controls.stop(); }
+                else if (commandupper.StartsWith("PAUSE")) { player.controls.pause(); }
                 else if (commandupper.StartsWith("POS"))
                 {
-                    string pos = functions.readPosition(player);
+                    string pos = player.controls.currentPositionString;
                     if (pos == "") { error("No file playing."); }
                     else { Console.WriteLine(pos); }
                 }
                 else if (commandupper.StartsWith("EXIT")) { Environment.Exit(0); }
                 else { error("Command unknown. Type 'help' for a list of commands."); }
             }
-        }
-        class functions
-        {
-            public static void play(WMPLib.WindowsMediaPlayer player, string @filepath)
-            {
-                try
-                {
-                    player.URL = filepath;
-                    player.controls.play();
-                }
-                catch (Exception err) { error("Error playing file: " + err.Message); }
-            }
-            public static void resume(WMPLib.WindowsMediaPlayer player) { player.controls.play(); }
-            public static void stop(WMPLib.WindowsMediaPlayer player){ player.controls.stop(); }
-            public static void pause(WMPLib.WindowsMediaPlayer player){ player.controls.pause(); }
-            public static string readPosition(WMPLib.WindowsMediaPlayer player){ return player.controls.currentPositionString; }
         }
     }
 }
