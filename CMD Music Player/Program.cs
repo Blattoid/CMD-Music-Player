54575545b0c3dd0de820a83575af9978d73fa994
floathandthing <springtrap.bite87@gmail.com>
2018-11-03 18:12:58 +0000

    My monkey brain figured out how to make progress bars work, so that has been implemented. There is also a function which returns a bool depending on whether a file has been selected or not.

--- CMD Music Player/Program.cs
+++ CMD Music Player/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace CMD_Music_Player
 {
@@ -9,38 +10,31 @@ namespace CMD_Music_Player
         public static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer(); //the heart of this program
 
         static bool bypassfilecheck = false;
-        static decimal Map(this decimal value, decimal fromSource, decimal toSource, decimal fromTarget, decimal toTarget)
-        {
-            return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
-        }
-        public static void generateProgressBar()
-        {
-            //CURRENTLY DOESNT WORK
-            try
-            {
-                int pos = Convert.ToInt32(player.controls.currentPosition);
-                int totallength = Convert.ToInt32(player.currentMedia.duration);
-                if (pos == 0) { Console.WriteLine("At beginning of media."); } //at beginning of track
-                else
-                {
-                    //draw the progress bar
-                    Console.Write("[");
-                    int progress = Convert.ToInt32(Map(pos, 0, totallength, 0, Console.WindowWidth- (player.controls.currentPositionString.Length + player.currentMedia.durationString.Length))); //calculate progress
-                    for (int i = 0; i <= progress-2; i++) { Console.Write("="); }
-                    Console.Write(">"); //arrow
-                    for (int i = 0; i <= Console.WindowWidth - progress - (player.controls.currentPositionString.Length+ player.currentMedia.durationString.Length)-6; i++) { Console.Write("."); } //2 for the [], 1 for the >, and however much else for the current/total time
-                    Console.Write("] ");
-                    //write the current and total time.
-                    Console.WriteLine(player.controls.currentPositionString + "/" + player.currentMedia.durationString);
-                }
-            }
-            catch (Exception) { error("No media playing."); }
-        }
         public static void error(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
             Console.ForegroundColor = ConsoleColor.White;
+        }
+        static bool IsMediaSelected()
+        {
+            bool answer;
+            try
+            {
+                string x = player.currentMedia.durationString;
+                answer = true;
+            }
+            catch { answer = false; }
+            return answer;
+        }
+        static string CreateBarFromMediaInfo()
+        {
+            //very simple; just returns a progress bar in a certain layout with the media information.
+            //this function only exists to prevent having to enter this very long command to get a standardised progress bar.
+            return ProgressBar.GenerateBar(player.controls.currentPosition,
+                                           player.currentMedia.duration,
+                                           barwidth: Console.WindowWidth-2,
+                                           barsuffix: "] " + player.controls.currentPositionString + "/" + player.currentMedia.durationString);
         }
         static void Main(string[] args)
         {
@@ -74,7 +68,7 @@ namespace CMD_Music_Player
                 {
                     string path = command.Substring(5);
                     //remove quotation marks, as they interfere with the media player.
-                    path = path.Replace(Convert.ToChar("\""), Convert.ToChar(""));
+                    path = path.Replace(Convert.ToChar("\""), Convert.ToChar(" "));
                     if (File.Exists(path) || bypassfilecheck) //check if either the file exists, or if said check is disabled.
                     {
                         Console.WriteLine("Playing " + path + "...");
@@ -92,13 +86,15 @@ namespace CMD_Music_Player
                 else if (commandupper.StartsWith("PAUSE")) { player.controls.pause(); }
                 else if (commandupper.StartsWith("POS"))
                 {
-                    try { Console.WriteLine(player.controls.currentPositionString + "/" + player.currentMedia.durationString); }
-                    catch (Exception err) { error("No media is loaded."); }
-                    //generateProgressBar();
+                    if (IsMediaSelected()) { Console.WriteLine(CreateBarFromMediaInfo()); }
+                    else { error("No media is loaded."); }
+                    
                 }
                 else if (commandupper.Replace(" ", "") == "GOTO") { error("Syntax: mm:ss"); }
                 else if (commandupper.StartsWith("GOTO"))
                 {
+                    if (!IsMediaSelected()) { error("No media is selected!"); continue; }
+
                     //player.controls.currentPosition is a double representing the number of seconds the track has been playing for.
                     command = command.Substring(5); //trim command + space
                     string[] strtimeargs = command.Split(Char.Parse(":"));
@@ -118,7 +114,8 @@ namespace CMD_Music_Player
                     if (timeargs.Count == 2) { seconds = (timeargs[0] * 60) + timeargs[1]; } //mm:ss
                     if (timeargs.Count == 3) { seconds = (timeargs[0] * 60 * 60) + (timeargs[1] * 60) + timeargs[2]; } //hh:mm:ss
                     player.controls.currentPosition = seconds;
-                    //generateProgressBar();
+
+                    Console.WriteLine(CreateBarFromMediaInfo());
                 }
 
                 else if (commandupper.StartsWith("TOGGLE_FILECHECK"))
@@ -131,6 +128,55 @@ namespace CMD_Music_Player
                 else if (command == "") { } //blank commands are ignored.
                 else { error("Command unknown. Type 'help' for a list of commands."); }
             }
+        }
+    }
+
+    static class ProgressBar
+    {
+        static decimal Map(this decimal value, decimal fromSource, decimal toSource, decimal fromTarget, decimal toTarget)
+        {
+            return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+        }
+        
+        //this was converted by hand from Python to C#.
+        public static string GenerateBar(double value, double maxvalue,
+            string barprefix = "[", string filledchar = "=", string pointerchar = ">",
+            string emptychar = " ", string barsuffix = "]", int barwidth = 50)
+        {
+            //sanity checks to make sure nothing is going to break anything
+            if (value > maxvalue)
+            {
+                Program.error("Current value is bigger than max value!");
+                return "";
+            }
+            if (value < 0)
+            {
+                Program.error("Current value is smaller than 0!");
+                return "";
+            }
+
+            decimal percentage = (decimal)value / (decimal)maxvalue * 100; //calculate the percentage of progress
+                                                           //calculate how much space the non-changing parts of the bar will take up
+            int length = (
+                barprefix.Length +
+                pointerchar.Length +
+                barsuffix.Length 
+            );
+
+            //If that length is smaller than the barwidth, then there will literally be no space left for the bar to move. Oh well.
+            //adjust the bar to fit the space it has, using the length we just calculated
+            value = Convert.ToInt32(Map((decimal)value, 0, (decimal)maxvalue, 0, barwidth - length));
+            maxvalue = barwidth - length;
+
+            //construct the bar as a string
+            string bar = barprefix; //start with the bar prefix
+            bar += String.Concat(Enumerable.Repeat(filledchar, (int)value)); //the currently filled portion of the bar
+            bar += pointerchar;
+            bar += String.Concat(Enumerable.Repeat(emptychar, (int)maxvalue - (int)value)); //the empty portion of the bar
+            bar += barsuffix + " "; //separator
+
+            return bar; //send the finished thing back
+
         }
     }
 }
