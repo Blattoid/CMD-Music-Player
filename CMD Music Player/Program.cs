8b9dacea9f6ee8c16ead8a2fbff1918ad24d4c3f
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-03-08 23:41:02 +0000

    Switches to using arrays for command parameters

--- CMD Music Player/Program.cs
+++ CMD Music Player/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace CMD_Music_Player
 {
@@ -33,7 +34,7 @@ namespace CMD_Music_Player
             //this function only exists to prevent having to enter this very long command to get a standardised progress bar.
             return ProgressBar.GenerateBar(player.controls.currentPosition,
                                            player.currentMedia.duration,
-                                           barwidth: Console.WindowWidth-2,
+                                           barwidth: Console.WindowWidth - 2,
                                            barsuffix: "] " + player.controls.currentPositionString + "/" + player.currentMedia.durationString);
         }
         static void Main(string[] args)
@@ -42,62 +43,75 @@ namespace CMD_Music_Player
             for (; ; )
             {
                 Console.Write(">");
-                string command = Console.ReadLine();
-                string commandupper = command.ToUpper();
+                string[] command = CommandLineToArgs(Console.ReadLine()/*.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)*/);
+                if (command.Length == 0) continue;
+                string commandupper = command[0].ToUpper();
 
-                if (commandupper.StartsWith("HELP"))
+                if (commandupper == "HELP")
                 {
-                    Console.WriteLine("List of commands:\n" +
-                                            "\tplay\n" +
-                                            "\tstop\n" +
-                                            "\tpause\n\n" +
-                                            "\tpos\n" +
-                                            "\tgoto\n\n" +
-                                            "\tls\n" +
-                                            "\texit"
-                    );  // don't be sad ;-;
+                    foreach (string line in new string[] { "List of commands:" ,
+                                             "\tplay" ,
+                                             "\tstop" ,
+                                             "\tpause" ,
+                                             "" ,
+                                             "\tpos" ,
+                                             "\tgoto" ,
+                                             "",
+                                            "\tls" ,
+                                            "\texit"})
+                    { Console.WriteLine(line); }
                 }
-                else if (commandupper.StartsWith("LS"))
+                else if (commandupper == "LS")
                 {
                     foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory()))
                         Console.WriteLine("\t" + Path.GetFileName(file));
                 }
-
-                else if (commandupper.Replace(" ", "") == "PLAY") { player.controls.play(); }
-                else if (commandupper.StartsWith("PLAY "))
+                else if (commandupper == "PLAY")
                 {
-                    string path = command.Substring(5);
-                    //remove quotation marks, as they interfere with the media player.
-                    path = path.Replace(Convert.ToChar("\""), Convert.ToChar(" "));
-                    if (File.Exists(path) || bypassfilecheck) //check if either the file exists, or if said check is disabled.
+                    if (command.Length == 1) { player.controls.play(); }
+                    else
                     {
-                        Console.WriteLine("Playing " + path + "...");
-                        try
+                        string path = command[1];
+                        //remove quotation marks, as they interfere with the media player.
+                        path = path.Replace(Convert.ToChar("\""), Convert.ToChar(" "));
+                        if (File.Exists(path) || bypassfilecheck) //check if either the file exists, or if said check is disabled.
                         {
-                            player.URL = path;
-                            player.controls.play();
+                            Console.WriteLine("Playing " + path + "...");
+                            try
+                            {
+                                player.URL = path;
+                                player.controls.play();
+                            }
+                            catch (Exception err) { error("Error playing file: " + err.Message); }
                         }
-                        catch (Exception err) { error("Error playing file: " + err.Message); }
+                        else { error("Error playing file: File doesn't exist."); }
                     }
-                    else { error("Error playing file: File doesn't exist."); }
                 }
-
-                else if (commandupper.StartsWith("STOP")) { player.controls.stop(); }
-                else if (commandupper.StartsWith("PAUSE")) { player.controls.pause(); }
-                else if (commandupper.StartsWith("POS"))
+                else if (commandupper == "STOP") { player.controls.stop(); }
+                else if (commandupper == "PAUSE") { player.controls.pause(); }
+                else if (commandupper == "POS")
                 {
-                    if (IsMediaSelected()) { Console.WriteLine(CreateBarFromMediaInfo()); }
+                    if (IsMediaSelected())
+                    {
+                        for (int i = 0; i < player.currentMedia.attributeCount; i++)
+                        {
+                            var name = player.currentMedia.getAttributeName(i);
+                            var value = player.currentMedia.getItemInfo(name);
+                            if (value.Length == 0) continue;
+                            Console.WriteLine(name + ": " + value);
+                        }
+
+                        Console.WriteLine(CreateBarFromMediaInfo());
+                    }
                     else { error("No media is loaded."); }
-                    
+
                 }
                 else if (commandupper.Replace(" ", "") == "GOTO") { error("Syntax: mm:ss"); }
-                else if (commandupper.StartsWith("GOTO"))
+                else if (commandupper == "GOTO")
                 {
                     if (!IsMediaSelected()) { error("No media is selected!"); continue; }
 
-                    //player.controls.currentPosition is a double representing the number of seconds the track has been playing for.
-                    command = command.Substring(5); //trim command + space
-                    string[] strtimeargs = command.Split(Char.Parse(":"));
+                    string[] strtimeargs = command[1].Split(Char.Parse(":"));
                     List<int> timeargs = new List<int>();
 
                     try
@@ -118,15 +132,42 @@ namespace CMD_Music_Player
                     Console.WriteLine(CreateBarFromMediaInfo());
                 }
 
-                else if (commandupper.StartsWith("TOGGLE_FILECHECK"))
+                else if (commandupper == "TOGGLE_FILECHECK")
                 {
                     //just for fun: disables checking of existence of a file. this could allow for lots of possible input, such as http addresses pointing to media
                     bypassfilecheck = !bypassfilecheck;
                     Console.WriteLine(!bypassfilecheck);
                 }
-                else if (commandupper.StartsWith("EXIT")) { Environment.Exit(0); }
-                else if (command == "") { } //blank commands are ignored.
+                else if (commandupper == "EXIT") { Environment.Exit(0); }
                 else { error("Command unknown. Type 'help' for a list of commands."); }
+            }
+        }
+        //https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs
+        );
+
+        public static string[] CommandLineToArgs(string commandLine)
+        {
+            int argc;
+            var argv = CommandLineToArgvW(commandLine, out argc);
+            if (argv == IntPtr.Zero)
+                throw new System.ComponentModel.Win32Exception();
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
             }
         }
     }
@@ -137,7 +178,7 @@ namespace CMD_Music_Player
         {
             return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
         }
-        
+
         //this was converted by hand from Python to C#.
         public static string GenerateBar(double value, double maxvalue,
             string barprefix = "[", string filledchar = "=", string pointerchar = ">",
@@ -154,13 +195,19 @@ namespace CMD_Music_Player
                 Program.error("Current value is smaller than 0!");
                 return "";
             }
+            if (maxvalue == 0)
+            {
+                Program.error("Error generating bar: max length is 0." +
+                    "");
+                return "";
+            }
 
             decimal percentage = (decimal)value / (decimal)maxvalue * 100; //calculate the percentage of progress
-                                                           //calculate how much space the non-changing parts of the bar will take up
+                                                                           //calculate how much space the non-changing parts of the bar will take up
             int length = (
                 barprefix.Length +
                 pointerchar.Length +
-                barsuffix.Length 
+                barsuffix.Length
             );
 
             //If that length is smaller than the barwidth, then there will literally be no space left for the bar to move. Oh well.
