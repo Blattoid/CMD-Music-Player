ec3a92fe8a9c822416512b3f44eb9b3a1fa52b93
Daniel Bridgewater <danielbridgewater87@gmail.com>
2019-03-14 21:51:14 +0000

    Adds file discovery functions along with infrastructure to support file indexing.

--- CMD Music Player/Program.cs
+++ CMD Music Player/Program.cs
@@ -9,7 +9,7 @@ namespace CMD_Music_Player
     static class Program
     {
         public static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer(); //the heart of this program
-
+        
         static bool bypassfilecheck = false;
         public static void error(string message)
         {
@@ -37,13 +37,39 @@ namespace CMD_Music_Player
                                            barwidth: Console.WindowWidth - 2,
                                            barsuffix: "] " + player.controls.currentPositionString + "/" + player.currentMedia.durationString);
         }
+
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
+
+            //scan list of folders
+            Console.WriteLine("Initialising music index...");
+            List<string> registeredfolders = new List<string> { }; //empty list to hold list of folders containing music
+            foreach (string path in
+                Properties.Settings.Default.RegisteredFolders.Split(
+                    new char[] { '^', '^', '^' }, //folder entries are separated by three carats.
+                    StringSplitOptions.RemoveEmptyEntries
+                )
+            ) registeredfolders.Add(path); //reconstruct list of folder paths from stored string
+            //check for music in the application folder if enabled.
+            if (Properties.Settings.Default.RegisterAppDirectory) registeredfolders.Add(AppDomain.CurrentDomain.BaseDirectory);
+
+            //Perform a recursive search on each folder for music files
+            Console.WriteLine("Scanning registered folders...");
+            List<string> discoveredfiles = new List<string> { };
+            foreach (string folder in registeredfolders)
+            {
+                foreach (string file in DataSearch.ScanForMedia(folder))
+                {
+                    discoveredfiles.Add(file);
+                }
+            }
+            Console.WriteLine(discoveredfiles.Count + " files found.");
+
             for (; ; )
             {
                 Console.Write(">");
-                string[] command = CommandLineToArgs(Console.ReadLine()/*.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)*/);
+                string[] command = CommandLineToArgs(Console.ReadLine());
                 if (command.Length == 0) continue;
                 string commandupper = command[0].ToUpper();
 
@@ -63,7 +89,7 @@ namespace CMD_Music_Player
                 }
                 else if (commandupper == "LS")
                 {
-                    foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory()))
+                    foreach (string file in discoveredfiles)
                         Console.WriteLine("\t" + Path.GetFileName(file));
                 }
                 else if (commandupper == "PLAY")
@@ -143,8 +169,8 @@ namespace CMD_Music_Player
                     }
                     //is this longer than the length of the song?
                     if (seconds > player.currentMedia.duration) error("Timecode is below 0 seconds.");
-                   //or is it smaller than 0 ? 
-                   player.controls.currentPosition = seconds;
+                    //or is it smaller than 0 ? 
+                    player.controls.currentPosition = seconds;
 
                     Console.WriteLine(CreateBarFromMediaInfo());
                 }
@@ -168,8 +194,7 @@ namespace CMD_Music_Player
         );
         public static string[] CommandLineToArgs(string commandLine)
         {
-            int argc;
-            var argv = CommandLineToArgvW(commandLine, out argc);
+            var argv = CommandLineToArgvW(commandLine, out int argc);
             if (argv == IntPtr.Zero)
                 throw new System.ComponentModel.Win32Exception();
             try
@@ -190,6 +215,55 @@ namespace CMD_Music_Player
         }
     }
 
+    static class DataSearch
+    {
+        public static List<string> ScanForMedia(string searchfolder, bool recursive = true, bool verbose = false)
+        {
+            List<string> found_files = new List<string> { }; //list to store discovered files.
+
+            //check if we have permission to access the folder
+            try { Directory.GetFiles(searchfolder); }
+            catch { return new List<string> { }; }
+
+            //add all the files in the folder that are music files.
+            foreach (string filepath in Directory.GetFiles(searchfolder))
+                {
+                    if (IsSupportedFiletype(filepath))
+                    {
+                        if (verbose) Console.WriteLine("Discovered " + filepath);
+                        found_files.Add(filepath);
+                    }
+                }
+
+            //recursively check all subdirectories if flag was set
+            if (recursive)
+            {
+                foreach (string folderpath in Directory.GetDirectories(searchfolder))
+                {
+                    if (verbose) Console.WriteLine("Traversing " + folderpath);
+                    List<string> sub_files = ScanForMedia(folderpath);
+                    foreach (string file in sub_files)
+                    {
+                        if (IsSupportedFiletype(file)) found_files.Add(file);
+                        
+                    }
+                }
+            }
+
+            //return the list of discovered files
+            if (verbose) Console.WriteLine(found_files.Count + " files found.");
+            return found_files;
+        }
+        static bool IsSupportedFiletype(string filename)
+        {
+            string[] supportedformats = new string[] { ".3g2", ".3gp", ".3gp2", ".3gpp", ".aac", ".adt", ".adts", ".aif", ".aifc", ".aiff", ".asf", ".asx", ".au", ".avi", ".cda", ".dvr-ms", ".flac", ".ivf", ".m1v", ".m2ts", ".m3u", ".m4a", ".m4v", ".mid", ".midi", ".mov", ".mp2", ".mp3", ".mp4", ".mp4v", ".mpa", ".mpe", ".mpeg", ".mpg", ".rmi", ".snd", ".wav", ".wax", ".wm", ".wma", ".wmd", ".wms", ".wmv", ".wmx", ".wmz", ".wpl", ".wvx" };
+            foreach (string type in supportedformats)
+            {
+                if (filename.EndsWith(type)) return true;
+            }
+            return false;
+        }
+    }
     static class ProgressBar
     {
         static decimal Map(this decimal value, decimal fromSource, decimal toSource, decimal fromTarget, decimal toTarget)
